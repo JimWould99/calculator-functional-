ab871ebcdc9488553f41455796616dcbb7d2fdf4
JimWould99 <130195351+JimWould99@users.noreply.github.com>
2024-01-07 07:01:51 +0000

    next draft

--- file.js
+++ file.js
@@ -1,10 +1,11 @@
 buttons = document.querySelectorAll('button')
 symbols = document.querySelectorAll('.symbol')
 
-let firstNumber = "0"
-let secondNumber = "0"
-let operator
-let start = true
+let firstNumber = 0
+let secondNumber = 0
+let operator = "none"
+let result
+let first = true
 let reset = false
 
 function add(number1, number2){
@@ -23,52 +24,107 @@ function divide(number1, number2){
     return number1 / number2
 }
 
-function operate(numberOne, numberTwo, numerator){
-   numberOne = parseFloat(firstNumber)
-   numberTwo = parseFloat(secondNumber)
-   let result
+function operate(first, second, numerator){
+  
+    firstNumber = parseFloat(first)
+    secondNumber = parseFloat(second)
+
    if (numerator == "div"){
-    result = divide(numberOne, numberTwo)
+    result = divide(firstNumber, secondNumber)
 } else if (numerator == "times") {
-    result = multiply(numberOne, numberTwo)
+    result = multiply(firstNumber, secondNumber)
 } else if (numerator == "plus") {
-    result = add(numberOne, numberTwo)
+    result = add(firstNumber, secondNumber)
 } else if (numerator == "minus") {
-    result = subtract(numberOne, numberTwo)
+    result = subtract(firstNumber, secondNumber)
 } 
-   console.log(`result: ${result}`)
-   secondNumber = "0"
-   firstNumber = result
-   operator = "none"
-   console.log(`newfirst: ${firstNumber}`)
+  first = true
+  reset = true
+  secondNumber = 0
+  firstNumber = result
+  console.log(result) 
+  return true
 } 
-
-
-
 
 buttons.forEach((button) => {
     button.addEventListener('click', function(e){
         selected = e.target
-        /*if (selected.getAttribute('class') == 'number') {
-            start = true
-        } */
-        if (selected.id == "equals") {
-            console.log(`final: ${firstNumber}`)
-            operate(firstNumber, secondNumber, operator)
-        }
+        console.log(first)
         if (selected.getAttribute('class') == 'symbol') {
             operator = selected.id
-            console.log('switch')
-            console.log(operator)
+            first = false
+            console.log(`operator: ${operator}`)
         }
-        if (selected.getAttribute('class') == 'number' && operator == "none") {
-                firstNumber += selected.innerHTML
-                console.log(`firstNumber: ${firstNumber}`)
-            } else {
-                secondNumber += selected.innerHTML
-                console.log(`secondNumber: ${secondNumber}`)
-            }
+        if (first && selected.getAttribute('class') == 'number' && reset == false) {
+            firstNumber += selected.innerHTML
+            console.log(`first: ${firstNumber}`)
+        } else if (!first && selected.getAttribute('class') == 'number') {
+            secondNumber += selected.innerHTML
+            console.log(`second: ${secondNumber}`)
+        }
+        if (selected.id == "equals") {
+            first = operate(firstNumber, secondNumber, operator)
+        }
+        if (selected.getAttribute('class') == 'number' && reset == true) {
+            reset = false
+            firstNumber = selected.innerHTML
+            console.log(`first: (other) ${firstNumber}`)
+        }
     });
+});
+
+/*
+logic2;
+need to somehow determine whether the input goes to the first number (replacing it).
+or goes to the second number afterwards
+
+1. condition1:
+whole calculator resets from answer
+2. condition2
+full monty- input all three value
+3. cond3
+repeat from answer into number1,
+
+condition1: 
+if user presses a number after display is showing an answer- will update number and process entirely
+condition2:
+if there is an answer and operator is pressed- the answer is carried over
+condition3:
+changes from number  one to two by virtue of pressing operator
+
+note- do not update the first number in operate into result. 
+do this in specific branch of display and operator 
+
+shape:
+1. check first whether answer is true or false (if display is showing final calculation)
+    1a. if answer is true and user clicks number 
+        1c. answer changed to false, (number changed to new number clicked, not appended )
+    1b. check whether answer is true and user clicks operator
+        1b. numberone becomes the result number
+    1.bb. answer true and 
+ 2. answer is false
+    2a. if number and thing;true append to firstNumber
+    2b. if operator then switch- thing;false
+    2c. if number and false: append to secondNumber
+
+    have global vairables for operator and two numbers
+
+1. if there is an operator, 
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*
